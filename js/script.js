eb6356aa2682457a04f24d1e75a779ed9342d621
rahil ahmadi <rahil.ahmadi2595@gmail.com>
2023-08-05 21:02:11 +0330

    create menu responsive

--- js/script.js
+++ js/script.js
@@ -11,8 +11,20 @@ starAndPrice.forEach(element => {
     // console.log(element.classList.contains('star_4'))
     if(element.classList.contains('star_4')==true){
         star_img.forEach(elem=>{
-            console.log(elem)
             elem.src='./img/star_4.svg';
         })
     }
 });
+
+// menu
+const open=document.querySelector('.menuopen');
+const close=document.querySelector('.menuClose');
+const layer=document.querySelector('.layer1');
+
+open.onclick=()=>{
+    layer.classList.add('check');
+}
+
+close.onclick=()=>{
+    layer.classList.remove('check');
+}
