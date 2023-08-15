a42aeb8d69bcf1d0361b0bfe092e1a88ed83af6c
rahil ahmadi <rahil.ahmadi2595@gmail.com>
2023-08-15 19:56:50 +0330

    add tab menu for our producting section

--- js/script.js
+++ js/script.js
@@ -28,3 +28,33 @@ open.onclick=()=>{
 close.onclick=()=>{
     layer.classList.remove('check');
 }
+
+// our products filter
+
+const indicatior=document.querySelector('.indicator').children;
+const main=document.querySelector('.items').children;
+
+
+for(let i=0; i<indicatior.length;i++)
+{
+    indicatior[i].onclick=function(){
+        for(let j=0;j<indicatior.length;j++){
+            indicatior[j].classList.remove('active_grouping_ourProduct_li')
+        }
+        this.classList.add('active_grouping_ourProduct_li');
+        const displayItem=this.getAttribute('data-fillter');
+        for (let z=0;z<main.length;z++){
+            main[z].style.transform='scale(0)';
+            setTimeout(() => {
+                main[z].style.display='none'
+            }, 500);
+            
+            if(main[z].getAttribute('data-category')==displayItem || displayItem=='all'){
+                main[z].style.transform='scale(1)';
+                setTimeout(() => {
+                    main[z].style.display='block'
+                }, 500);
+            }
+        }
+    }
+}
